d39d4144889494a4965eb1e4b8dfe45f8a896318
Alexandr Zhavoronkov <sasazhava13@gmail.com>
2022-03-02 13:59:59 +0300

    added lab to labs-list

--- index.php
+++ index.php
@@ -8,13 +8,17 @@
   <div id="wrapper">
     <?php $header_text='Лабораторные работы по предмету <b>"Основы серверной веб-разработки"' ?>
     <?php include("template/header.php") ?>
-    <main class="index-main">
+    <main class="index-main" style="flex-direction: row">
     <?php
       $labs = array(
         array(
           "github_link" => "https://github.com/AlexZhaba/o-my-god-it-is-php/blob/master/index.php",
           "host_link" => "http://phplabs.std-1734.ist.mospolytech.ru/index.php",
         ),
+        array(
+          "github_link" => "https://github.com/AlexZhaba/o-my-god-it-is-php/tree/master/lab2",
+          "host_link" => "http://phplabs.std-1734.ist.mospolytech.ru/lab2/index.php",
+        )
       );
 
       for ($i = 0; $i < count($labs); ++$i) {
@@ -25,8 +29,8 @@
         echo "
           <div class='container lab'>
             <div>Лабораторная работа №$index</div>
-            <a href='$github_link'>Github</a>
-            <a href='$host_link'>Host</a>
+            <a href='$github_link' target='_blank'>Github</a>
+            <a href='$host_link' target='_blank'>Host</a>
           </div>
         ";
       }
