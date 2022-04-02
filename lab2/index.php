b3c25149aca678a4ab1171e088301498bbd285af
Alexandr Zhavoronkov <sasazhava13@gmail.com>
2022-04-02 16:12:38 +0300

    fix lab2

--- lab2/index.php
+++ lab2/index.php
@@ -15,6 +15,22 @@
         <input type="text" name="name" required>
         <label for="email">Ваша почта:</label>
         <input type="email" name="email" required>
+        <select name="question_type">
+          <option value="report">Жалоба</option>
+          <option value="offer">Предложение</option>
+          <option value="thanks">Благодарность</option>
+        </select>
+        <p for="answer_type">Вариант ответа:</p>
+        <div class="container-form">
+          <input type="radio" id="contactChoice1"
+            name="contact" value="email">
+          <label for="contactChoice1">Email</label>
+        </div>
+        <div class="container-form">
+          <input type="radio" id="contactChoice2"
+            name="contact" value="phone">
+          <label for="contactChoice2">Phone</label>
+        </div>
         <label for="question">Ваш вопрос:</label>
         <textarea name="question" cols="30" rows="10"></textarea>
         <button>Отправить</button>
